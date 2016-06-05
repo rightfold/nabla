2d5fe23c55c39bf3567e44f0bd320c4a09ea1bb5
rightfold <rightfold@gmail.com>
2016-06-05 13:24:01 +0200

    Implement class for communicating with server

---
+++ js/nabla.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Server {
+    constructor() {
+        this.worker = new Worker('output/nabla-server.js');
+        this.worker.addEventListener('message', ev => {
+            const {id, result} = JSON.parse(ev.data);
+            const callback = this.callbacks.get(id);
+            this.callbacks.delete(id);
+            callback(result);
+        });
+        this.nextID = 0;
+        this.callbacks = new Map();
+    }
+
+    dispose() {
+        this.worker.terminate();
+    }
+
+    compute(formula, callback) {
+        const id = this.nextID++;
+        this.callbacks.set(id, callback);
+        this.worker.postMessage(JSON.stringify({id, formula}));
+    }
+}
+
+addEventListener('load', () => {
+    const server = new Server();
+    server.compute('x', result => {
+        console.log(result);
+    });
+});
