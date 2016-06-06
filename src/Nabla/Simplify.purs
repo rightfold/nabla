e7e4b2c67712048bc3ff961e28b6604671719b07
Radek Slupik <rightfold@gmail.com>
2016-06-06 11:23:38 +0200

    Simplify multiplication of terms by themselves

--- src/Nabla/Simplify.purs
+++ src/Nabla/Simplify.purs
@@ -14,6 +14,7 @@ simplify x = if simplified == x then x else simplify simplified
 simplify' :: Term -> Term
 simplify' (App Add [x, y]) | x == y = App Mul [Num 2, simplify' x]
 simplify' (App Add xs) = App Add (xs # map simplify' # Array.sort)
+simplify' (App Mul [x, y]) | x == y = App Pow [simplify' x, Num 2]
 simplify' (App Mul xs) = App Mul (xs # map simplify' # Array.sort)
 simplify' (App f xs) = App (simplify' f) (map simplify' xs)
 simplify' t = t
