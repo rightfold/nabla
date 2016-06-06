876978ac511f8bd55815f6ced46aa594a6341f09
Radek Slupik <rightfold@gmail.com>
2016-06-06 11:17:06 +0200

    Simplify all calls

--- src/Nabla/Simplify.purs
+++ src/Nabla/Simplify.purs
@@ -8,10 +8,12 @@ import Nabla.Term (Term(..))
 import Prelude
 
 simplify :: Term -> Term
-simplify = simplify'
+simplify x = if simplified == x then x else simplify simplified
+  where simplified = simplify' x
 
 simplify' :: Term -> Term
 simplify' (App Add [x, y]) | x == y = App Mul [Num 2.0, simplify' x]
 simplify' (App Add xs) = App Add (xs # map simplify' # Array.sort)
 simplify' (App Mul xs) = App Mul (xs # map simplify' # Array.sort)
+simplify' (App f xs) = App (simplify' f) (map simplify' xs)
 simplify' t = t
