6b5ffa252a1e707e2cc8233cd72c6a8d82ca040f
rightfold <rightfold@gmail.com>
2016-06-07 21:18:19 +0200

    Eliminate identity elements introduced by constant folding

--- src/Nabla/Simplify.purs
+++ src/Nabla/Simplify.purs
@@ -17,8 +17,8 @@ simplify' :: Term -> Term
 simplify' (App f xs) =
   App (simplify' f) (simplify' <$> xs)
   # simplifyAssociativity
-  # simplifyIdentity
   # simplifyConstants
+  # simplifyIdentity
   # simplifyCommutativity
   # simplifyUnaryApp
   # simplifyZeroProduct
@@ -29,17 +29,6 @@ simplifyAssociativity (App f xs) | associative f = App f (xs >>= flatten)
   where flatten (App g ys) | g == f = ys
         flatten t = [t]
 simplifyAssociativity t = t
-
-simplifyIdentity :: Term -> Term
-simplifyIdentity (App f []) =
-  case identity f of
-    Nothing -> App f []
-    Just x  -> x
-simplifyIdentity (App f xs) =
-  case identity f of
-    Nothing -> App f xs
-    Just x  -> App f (Array.filter (_ /= x) xs)
-simplifyIdentity t = t
 
 simplifyConstants :: Term -> Term
 simplifyConstants (App f xs) =
@@ -52,6 +41,17 @@ simplifyConstants (App f xs) =
           where go {consts, rest} (Num x) = {consts: Array.cons x consts, rest}
                 go {consts, rest} t       = {consts, rest: Array.cons t rest}
 simplifyConstants t = t
+
+simplifyIdentity :: Term -> Term
+simplifyIdentity (App f []) =
+  case identity f of
+    Nothing -> App f []
+    Just x  -> x
+simplifyIdentity (App f xs) =
+  case identity f of
+    Nothing -> App f xs
+    Just x  -> App f (Array.filter (_ /= x) xs)
+simplifyIdentity t = t
 
 simplifyCommutativity :: Term -> Term
 simplifyCommutativity (App f xs) | commutative f = App f (Array.sort xs)
@@ -70,15 +70,15 @@ associative Add = true
 associative Mul = true
 associative _ = false
 
-identity :: Term -> Maybe Term
-identity Add = Just (Num zero)
-identity Mul = Just (Num one)
-identity _ = Nothing
-
 foldConstants :: Term -> Maybe {op :: BigInt -> BigInt -> BigInt, id :: BigInt}
 foldConstants Add = Just {op: (+) :: BigInt -> BigInt -> BigInt, id: zero :: BigInt}
 foldConstants Mul = Just {op: (*) :: BigInt -> BigInt -> BigInt, id: one :: BigInt}
 foldConstants _ = Nothing
+
+identity :: Term -> Maybe Term
+identity Add = Just (Num zero)
+identity Mul = Just (Num one)
+identity _ = Nothing
 
 commutative :: Term -> Boolean
 commutative Add = true
