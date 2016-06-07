7c62644b5741895f9c0d84f89ce0a48d1e87a123
rightfold <rightfold@gmail.com>
2016-06-07 19:48:45 +0200

    Sort operands to commutative operators

--- src/Nabla/Simplify.purs
+++ src/Nabla/Simplify.purs
@@ -2,6 +2,7 @@ module Nabla.Simplify
 ( simplify
 ) where
 
+import Data.Array as Array
 import Nabla.Term (Term(..))
 import Prelude
 
@@ -12,7 +13,9 @@ simplify x = if simplified == x then x else simplify simplified
 simplify' :: Term -> Term
 simplify' (App f xs) = App f' xs'
   where f'  =     simplify' f
-        xs' = map simplify xs # if flat f' then (_ >>= flatten) else id
+        xs' = map simplify xs
+              # (if flat f' then (_ >>= flatten) else id)
+              # (if orderless f' then Array.sort else id)
         flatten t@(App g xs) | g == f'   = xs
                              | otherwise = [t]
         flatten t = [t]
@@ -22,3 +25,8 @@ flat :: Term -> Boolean
 flat Add = true
 flat Mul = true
 flat _ = false
+
+orderless :: Term -> Boolean
+orderless Add = true
+orderless Mul = true
+orderless _ = false
